34775a40cd4a92057aba319a1deb6b069e040f29
unknown <flatpita92@gmail.com>
2016-01-21 22:23:08 -0600

    updated social links

--- PersonalSite.py
+++ PersonalSite.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template
 
+import smtplib
+
 app = Flask(__name__)
 
 
@@ -7,6 +9,25 @@ app = Flask(__name__)
 def hello_world():
     return render_template('index.html')
 
+@app.route('/contact', methods=['GET','POST'])
+def email():
+    sender = 'flatpita92@gmail.com'
+    receivers = ['joe@joeracosta.com']
+
+    message = """From: From Person <from@fromdomain.com>
+    To: To Person <to@todomain.com>
+    Subject: SMTP e-mail test
+
+    This is a test e-mail message.
+    """
+
+    try:
+        smtpObj = smtplib.SMTP('localhost', 5000)
+        smtpObj.sendmail(sender, receivers, message)
+        print "Successfully sent email"
+    except smtplib.SMTPException:
+        print "Error: unable to send email"
+    return
 
 if __name__ == '__main__':
-    app.run(host='192.168.1.4')
+    app.run(host='localhost')
